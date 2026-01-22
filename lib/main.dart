fc293c0406bbdd98fd7da7c2ccbd96d30e1b0c0b
vasuramji52 <vasupradha.ramji@gmail.com>
2026-01-22 14:18:41 -0500

    Test data populated for routers

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+// TODO Implement this library.
+
+import 'package:flutter/material.dart';
+import 'package:job_entry/router.dart';
+import 'package:css/css.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: CSS.pinkTheme,
+      home: RouterScreen(
+        size: MediaQuery.sizeOf(context)
+      ),
+    );
+  }
+}
